1ac52a129121d08037cf12c357fb167a23119b7f
EduardoVital <eduardovitals@gmail.com>
2023-01-30 18:30:20 -0300

    Add dynamic data from github api

--- Project-4/Challenge/GithubBlog/src/pages/Blog/components/ProfileCard/index.tsx
+++ Project-4/Challenge/GithubBlog/src/pages/Blog/components/ProfileCard/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 import {
   Card,
   CardContainer,
@@ -9,42 +10,74 @@ import IconArrowUp from '../../../../assets/icons/IconArrowUp.svg'
 import IconGithub from '../../../../assets/icons/IconGithub.svg'
 import IconBuilding from '../../../../assets/icons/IconBuilding.svg'
 import IconUserGroup from '../../../../assets/icons/IconUserGroup.svg'
+import { api } from '../../../../lib/axios'
+import { useEffect, useState } from 'react'
+
+interface UserData {
+  name: string
+  login: string
+  company: string
+  followers: number
+  url: string
+  avatar_url: string
+  bio: string
+}
 
 export function ProfileCard() {
+  const [userData, setUserData] = useState<UserData>()
+
+  const userName = 'EduardoVital'
+
+  const getData = async () => {
+    await api.get(`/users/${userName}`).then((response) => {
+      const { name, company, followers, html_url, avatar_url, login, bio } =
+        response.data
+      setUserData({
+        name,
+        company,
+        followers,
+        url: html_url,
+        avatar_url,
+        login,
+        bio,
+      })
+    })
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
   return (
     <Card>
       <CardContainer>
         <ImageDiv>
-          <img src="https://github.com/EduardoVital.png" alt="" />
+          <img src={userData?.avatar_url} alt="" />
         </ImageDiv>
 
         <Description>
           <div>
-            <h1>Eduardo Vital de Souza</h1>
-            <a href="">
+            <h1>{userData?.name}</h1>
+            <a href={userData?.url} target="_blank" rel="noreferrer">
               GITHUB
               <img src={IconArrowUp} alt="" />
             </a>
           </div>
 
-          <p>
-            Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
-            viverra massa quam dignissim aenean malesuada suscipit. Nunc,
-            volutpat pulvinar vel mass.
-          </p>
+          <p>{userData?.bio}</p>
 
           <MediaInformation>
             <span>
               <img src={IconGithub} alt="" />
-              cameronwll
+              {userData?.login}
             </span>
             <span>
               <img src={IconBuilding} alt="" />
-              Rocketseat
+              {userData?.company}
             </span>
             <span>
               <img src={IconUserGroup} alt="" />
-              32 seguidores
+              {userData?.followers} seguidores
             </span>
           </MediaInformation>
         </Description>
