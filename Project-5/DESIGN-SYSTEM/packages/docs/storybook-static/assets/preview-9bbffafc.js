e41ec0ffcf318c7b486c25da687ff35185dfbf4b
EduardoVital <eduardovitals@gmail.com>
2023-04-03 12:52:37 -0300

    Add gitignore

--- Project-5/DESIGN-SYSTEM/packages/docs/storybook-static/assets/preview-9bbffafc.js
+++
@@ -1,2 +0,0 @@
-import{t as o}from"./index-0d339697.js";import"./iframe-e108cb48.js";import"./es.map.constructor-0df5ce3a.js";import"./es.number.to-fixed-37af5609.js";import"./index-c7a1a5ee.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
-//# sourceMappingURL=preview-9bbffafc.js.map
